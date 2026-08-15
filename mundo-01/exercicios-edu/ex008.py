bd8f1959af914094e1d419f8cfc3036259baf62d
Eduardo Ozoterio <170747105+eduozt@users.noreply.github.com>
2026-08-15 15:54:18 -0300

    edit 8 e 9

--- mundo-01/exercicios-edu/ex008.py
+++ mundo-01/exercicios-edu/ex008.py
@@ -1,4 +1,8 @@
 m = float(input('Digite um valor em metros: '))
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
 cm = m * 100
-mm = cm * 100
-print('{} metros equivalem a {} centímetros ou a {} milímetros.'.format(m, cm, mm))
+mm = m * 1000
+print('{} m equivalem a: \n{:.2f} km \n{:.2f} hm \n{:.2f} dam \n{:.2f} dm \n{:.2f} cm \n{:.2f} mm'.format(m, km, hm, dam, dm, cm, mm))
