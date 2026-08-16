2deb18f460dc71d753adf886ed53792338216042
Eduardo Ozoterio <170747105+eduozt@users.noreply.github.com>
2026-08-16 08:47:38 -0300

    aula 08a

---
+++ mundo-01/python-teste/aula08a.py
@@ -0,0 +1,22 @@
+"""
+Importar pacote completo: import package
+Importar partes: from package import funcao1, funcao2
+
+import math
+
+ceil: arrendondar para cima
+floor: arredondar para baixo
+sqrt: raiz quadrada
+
+"""
+
+import math
+
+print(math.sqrt(25))
+print(math.pi)
+print(math.ceil(2.3))
+
+import random
+
+print(random.randint(10,20))
+
